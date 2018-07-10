1be0c1cf9e729baa38e1388048a6fe8acd4fcc17
Grzegorz Szadkowski <5392918+gszadkow@users.noreply.github.com>
2018-07-10 13:55:52 +0200

    #5: Splash screen

---
+++ android/app/src/main/java/net/edoproject/loco/SplashActivity.java
@@ -0,0 +1,34 @@
+package net.edoproject.loco;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent openIntroActivity =  new Intent("android.intent.action.INTRO");
+                    startActivity(openIntroActivity);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
